7780651833a7875434c28072c41d05317c3308f6
Breno Moura <breno.mm@outlook.com>
2024-07-31 21:24:34 -0300

    Update ohmyzsh installation

--- install_1.sh
+++ install_1.sh
@@ -4,4 +4,4 @@ apt update
 
 apt install tmux zsh curl -y
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-sh -c -n "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
